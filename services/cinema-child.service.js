ab2ac13ecf5741f9cc210fb524010007ea3dbf89
pattiespearsL2k <luongngocjunsu@gmail.com>
2022-07-04 22:03:49 +0700

    update api

--- services/cinema-child.service.js
+++ services/cinema-child.service.js
@@ -28,7 +28,6 @@ const getCinemaIDByRoomID = async(roomID) => {
     const cinemaChild = await CinemaChild.findOne({'listRoom.roomID' : roomID});
     return cinemaChild.cinemaID;
 }
-
 module.exports = {
     getCumRapTheoMaHeThong,
     getTenRapByMaRap,
